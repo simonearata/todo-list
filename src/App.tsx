f6eb06166a903206f8054d71497d0074599744a7
Simone <simone.arata1@gmail.com>
2021-11-24 17:49:25 +0100

    context

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import "./App.css";
 import Board from "./components/board";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CategoryProvider from "./providers/category-provider";
+import NoteProvider from "./providers/note-provider";
 
 function App() {
   return (
     <div className="App">
-      <Board name="ToDoList" />
+      <NoteProvider>
+        <CategoryProvider>
+          <Board name="ToDoList" />
+        </CategoryProvider>
+      </NoteProvider>
     </div>
   );
 }
